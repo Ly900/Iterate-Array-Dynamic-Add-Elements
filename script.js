aaf08403591848db6e133f278b6347cfa8215537
Ly Nguyen <Ly900@aol.com>
2015-10-28 09:17:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ $(document).ready(function(){
 
   var Array = ["A", "B", "C", "D"];
 
-  var divChildren = $(".board").children();
-
   $.each(Array, function (index, value) {
 
       $("<div class='letterDiv'> </div>").html(value).appendTo(".board");
